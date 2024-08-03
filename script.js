30d536dda69afd9db29d6d135d0a000182113f29
Abdulaziz Ergashev <eabdulaziz36@gmail.com>
2024-08-03 10:48:26 +0500

    Expenses and Time Tasks

---
+++ script.js
@@ -0,0 +1,68 @@
+// TASK 1 ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| 
+
+// let USD = 11000.34;
+// let EURO = 12354.03;
+
+// let ticket = 500;
+// let hotel = 250;
+// let entertainment = 120;
+
+// let conversionFromEuroToUzs = entertainment * EURO;
+// let conversionFromUsdToUzs = (ticket + hotel) * USD;
+// let totalExpenditureInUzs = conversionFromEuroToUzs + conversionFromUsdToUzs;
+
+// console.log(totalExpenditureInUzs);
+// let balance = prompt('Sayohat uchun ajratilgan summani UZSda kiriting');
+
+// if (balance >= totalExpenditureInUzs) {
+//     console.log(`Oq yo'l, Alisher!`);
+// } else {
+//     console.log(`Alisher, ozgina sabr qilish kerak bo'lar ekan`);
+// }
+
+
+
+
+
+// TASK 2 ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| 
+
+// let distance = prompt('Masofani kilometrda kiriting');
+
+// if (!isNaN(distance)) {
+//     let planeTime = distance / 800;
+//     let planeHours = Math.floor(planeTime);
+//     let planeMinutes = Math.floor((planeTime - planeHours) * 60);
+//     let planeSeconds = Math.floor(((planeTime - planeHours) * 60 - planeMinutes) * 60);
+
+//     console.log(`Ushbu masofani samolyot ${planeHours} soat, ${planeMinutes} minut, ${planeSeconds} sekundda bosib o'tadi`);
+
+//     let carTime = distance / 80;
+//     let carHours = Math.floor(carTime);
+//     let carMinutes = Math.floor((carTime - carHours) * 60);
+//     let carSeconds = Math.floor(((carTime - carHours) * 60 - carMinutes) * 60);
+
+//     console.log(`Ushbu masofani mashina ${carHours} soat, ${carMinutes} minut, ${carSeconds} sekundda bosib o'tadi`);
+
+//     let bikeTime = distance / 20;
+//     let bikeHours = Math.floor(bikeTime);
+//     let bikeMinutes = Math.floor((bikeTime - bikeHours) * 60);
+//     let bikeSeconds = Math.floor(((bikeTime - bikeHours) * 60 - bikeMinutes) * 60);
+
+//     console.log(`Ushbu masofani velosiped ${bikeHours} soat, ${bikeMinutes} minut, ${bikeSeconds} sekundda bosib o'tadi`);
+
+//     let onfootTime = distance / 5;
+//     let onfootHours = Math.floor(onfootTime);
+//     let onfootMinutes = Math.floor((onfootTime - onfootHours) * 60);
+//     let onfootSeconds = Math.floor(((onfootTime - onfootHours) * 60 - onfootMinutes) * 60);
+
+//     console.log(`Ushbu masofani piyoda ${onfootHours} soat, ${onfootMinutes} minut, ${onfootSeconds} sekundda bosib o'tadi`);
+
+// } else {
+//     console.log('Siz string kiritidingiz!');
+// }
+
+
+
+
+
+
